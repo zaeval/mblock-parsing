e4d9c4863be81416741024689a66dbb59491255a
zaeval <h3065@naver.com>
2019-10-22 14:54:38 +0900

    로그인, 회원가입, 프로젝트 리스트 받아오기 완료

--- main.py
+++ main.py
@@ -5,12 +5,20 @@ ROOT_URL = "https://ide.mblock.cc/#/"
 NEW_PROJECT_URL = ROOT_URL + "projects/projects"
 PROJECT_CHANGE_SAVE_URL = ROOT_URL + "web/saveas"
 
-API_URL = "https://eu.passport2.makeblock.com/v1/"
-LOGIN_URL = API_URL + "user/login"
-EMAIL_REGISTER = "user/emailregisternocode"
+AUTHORIZE_API_URL = "https://eu.passport2.makeblock.com/v1"
+LOGIN = "/user/login"
+EMAIL_REGISTER = "/user/emailregisternocode"
 
 account = "chl8273@naver.com"
 password = "chlworkd13"
+
+CONTENT_API_URL = "https://planet.mblock.cc"
+LIST = "/cloud/pro/list"
+GET_PROJECT = "/getprojectbycpids"
+SHARE = "/project/ThirdPlatform"
+# https://ide.mblock.cc/share.html?98628
+# 85471
+# blob:https://ide.mblock.cc/7c34c362-3302-490d-9848-d36f485b0f24
 
 header = {
     "Referer": "https://ide.mblock.cc/",
@@ -19,7 +27,7 @@ header = {
 
 
 def regist(ses, account, password):
-    ses.post(API_URL + EMAIL_REGISTER, data={"email": account, "password": password})
+    ses.post(AUTHORIZE_API_URL + EMAIL_REGISTER, data={"email": account, "password": password})
 
 
 def login(ses, account, password):
@@ -27,22 +35,11 @@ def login(ses, account, password):
         "Referer": "https://ide.mblock.cc/",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"
     }
-    res = ses.post(LOGIN_URL, data={
+    res = ses.post(AUTHORIZE_API_URL + LOGIN, data={
         "account": account,
         "password": password
     }, headers=header)
-    # cookie = res.headers['Set-Cookie'][:89]
-    # print(cookie)
-    # headers = {"Cookie": cookie}
-    print(res.text)
-    # response = s.get(ROOT_URL, headers=headers)
-    # print(response.text)
-    print(res.text)
-    print(res.status_code)
-    # response = s.get(ROOT_URL, headers=headers)
-    # print(response.text)
-
-    response = s.get(ROOT_URL, headers=header)
+    return res
 
 
 # def save(ses):
@@ -50,30 +47,31 @@ def login(ses, account, password):
 # print(res.text)
 
 
-def get_list(ses):
-    res = ses.post("https://planet.mblock.cc/cloud/pro/list")
-    print(res.text)
+def getList(ses, token):
+    headers = {'token': token, 'utoken': token}
+
+    res = ses.post(CONTENT_API_URL + LIST, headers=headers)
+    return res
 
 
-s = requests.session()
-# regist(s, account, password)
-# login(s, account, password)
-res = s.post(LOGIN_URL, data={
-    "account": account,
-    "password": password
-}, headers=header)
+def share(ses, token, id):
+    headers = {'token': token, 'utoken': token, 'accesstoken': None}
+    res = ses.post(CONTENT_API_URL + SHARE, headers=headers, data={{"cloudProjectId": id}})
+    return res
+
+
+session = requests.session()
+res = login(session, account, password)
+res = json.loads(res.text)
+
+token = res["data"]["token"]
+res = getList(session, token)
+res = json.loads(res.text)
+
+id = res["data"]["cloudProjectList"][1]['id']
+
+res = share(session, token, int(id))
 print(res.text)
-res_header = json.loads(res.text)['data']
-print(res_header['token'])
-header = {
-    "token": res_header['token'],
-    "utoken": res_header['token'],
-}
-print(header)
-# cookie = dict(res.headers['Set-Cookie'][:89])
-# print(cookie)
-# headers = {"Cookie": cookie}
-response = s.get(ROOT_URL, headers=header)
-print(response)
-test = s.get("https://planet.mblock.cc/cloud/token/coverupload")
-print(test.text)
+# print(res["data"])
+# test = s.get("https://planet.mblock.cc/cloud/token/coverupload")
+# print(test.text)
